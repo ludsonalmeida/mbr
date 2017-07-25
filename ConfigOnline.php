0613edd68073165359f32d91818d73ad5dee5dab
Ludson Almeida <ludson.bsa@gmail.com>
2017-07-25 20:22:15 -0300

    tratamento de erros

--- ConfigOnline.php
+++ ConfigOnline.php
@@ -13,4 +13,31 @@ define('EMAIL_PASS','teste');
 define('EMAIL_PORT',587);
 define('EMAIL_HOST','smtp.gmail.com');
 
-?>
+define('MBR_ACCEPT','accept');
+define('MBR_INFOR','infor');
+define('MBR_ALERT','alert');
+define('MBR_ERROR','error');
+
+function MBRError($ErrMsg, $ErrNo, $ErrDie = null) {
+$CssClass = ($ErrNo == E_USER_NOTICE ? MBR_INFOR : ($ErrNo == E_USER_WARNING ? MBR_ALERT : ($ErrNo == E_USER_ERROR ? MBR_ERROR : $ErrNo)));
+    echo "<p class=\"trigger {$CssClass}\">{$ErrMsg}<span class=\"ajax_close\"></span></p>";
+
+    if ($ErrDie):
+        die;
+    endif;
+}
+
+//PHPErro :: personaliza o gatilho do PHP
+function PHPErro($ErrNo, $ErrMsg, $ErrFile, $ErrLine) {
+    $CssClass = ($ErrNo == E_USER_NOTICE ? MBR_INFOR : ($ErrNo == E_USER_WARNING ? MBR_ALERT : ($ErrNo == E_USER_ERROR ? MBR_ERROR : $ErrNo)));
+    echo "<p class=\"trigger {$CssClass}\">";
+    echo "<b>Erro na Linha: #{$ErrLine} ::</b> {$ErrMsg}<br>";
+    echo "<small>{$ErrFile}</small>";
+    echo "<span class=\"ajax_close\"></span></p>";
+
+    if ($ErrNo == E_USER_ERROR):
+        die;
+    endif;
+}
+
+set_error_handler('PHPErro');
